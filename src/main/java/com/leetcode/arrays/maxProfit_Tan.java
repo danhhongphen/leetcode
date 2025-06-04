11427ab917584468135fe007d1c76fa93d762c4a
Trần Đình Tân <118046109+dinhtan131@users.noreply.github.com>
2025-06-04 17:07:39 +0700

    Update maxProfit_Tan.java
    Add solution 2

--- src/main/java/com/leetcode/arrays/maxProfit_Tan.java
+++ src/main/java/com/leetcode/arrays/maxProfit_Tan.java
@@ -1,5 +1,5 @@
 package com.leetcode.arrays;
-
+import java.util.Arrays;
 public class maxProfit_Tan {
     public int maxProfit(int[] prices) {
     int maxProfit = 0;
@@ -15,14 +15,38 @@ public class maxProfit_Tan {
     }
     public int maxProfit1(int[] prices) {
     int maxProfit = 0;
-    for (int i = 0; i < prices.length - 1; i++) {
-        for (int j = i + 1; j < prices.length; j++) {
-            int profit = prices[j] - prices[i];
-            if (profit > maxProfit) {
-                maxProfit = profit;
-            }
+    int minBuy=100;
+    for (int i = 0; i < prices.length; i++) {
+        if (minBuy > prices[i]) {
+            minBuy=prices[i];
+        }
+        int maxProfitTemp = prices[i] - minBuy;
+        if(maxProfitTemp>maxProfit)
+        {
+            maxProfit=maxProfitTemp;
         }
     }
     return maxProfit;
     }
+    
+    public static void main(String[] args) {
+    maxProfit_Tan profit=new maxProfit_Tan();
+    int[] arr1 = {10,1,5,6,7,1};
+    int[] arr2 = {10,8,7,5,2};
+    int[] arr3 = {};
+    
+    System.out.println("Check array 1"); 
+    System.out.println("Solution 1: " + profit.maxProfit(arr1)); 
+    System.out.println("Solution 2: " + profit.maxProfit1(arr1)); 
+
+    
+    System.out.println("\nCheck array 2"); 
+    System.out.println("Solution 1: " + profit.maxProfit(arr2)); 
+    System.out.println("Solution 2: " + profit.maxProfit1(arr2)); 
+
+    System.out.println("\nCheck array 3"); 
+    System.out.println("Solution 1: " + profit.maxProfit(arr3)); 
+    System.out.println("Solution 2: " + profit.maxProfit1(arr3)); 
+    }
+
 }
