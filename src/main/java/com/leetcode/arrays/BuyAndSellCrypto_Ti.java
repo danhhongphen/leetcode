748169344ed02fd2a1b2e58891e767237b973735
quocti2003 <lejack526@gmail.com>
2025-06-04 13:56:00 +0700

    i dont know why this simple but i can not nghi ra, shiba

--- src/main/java/com/leetcode/arrays/BuyAndSellCrypto_Ti.java
+++ src/main/java/com/leetcode/arrays/BuyAndSellCrypto_Ti.java
@@ -23,4 +23,20 @@ public class BuyAndSellCrypto_Ti {
         }
         return profit;
     }
+    // Mehthod: super simple but i don't know why i can not think like this :"(
+    public int maxProfit_2(int[] prices) {
+        int buy_price = prices[0];
+        int profit = 0;
+        for (int i = 1; i < prices.length; ++i){
+            if (prices[i] < buy_price)
+            {
+                buy_price = prices[i];
+            }
+            else if (prices[i] - buy_price > profit)
+            {
+                profit = prices[i] - buy_price;
+            }
+        }
+        return profit;
+    }
 }
