36a5f6f0142b0ebd17dea1b04c13b1b0f477c736
quocti2003 <lejack526@gmail.com>
2025-06-04 10:56:03 +0700

    update 2 methods for valid palindrome problems

--- src/main/java/com/leetcode/arrays/ValidPalindrome_Ti.java
+++ src/main/java/com/leetcode/arrays/ValidPalindrome_Ti.java
@@ -1,6 +1,8 @@
 package com.leetcode.arrays;
 
 public class ValidPalindrome_Ti {
+    // Method1: Myself
+    // [^] --> means if not some value in square bracket
     public boolean isPalindrome(String s) {
         String regex = "[^a-zA-z0-9]";
         s = s.trim().toLowerCase().replaceAll(regex, "");
@@ -16,19 +18,64 @@ public class ValidPalindrome_Ti {
         return true;
     }
 
+    // Method2: (after watching hint in leetcode)
+    public boolean isPalindrome_2(String s) {
+        String regex = "[^a-zA-z0-9]";
+        s = s.trim().toLowerCase().replaceAll(regex, "");
+        int sLength = s.length();
+        int currentStart = 0;
+        int currentEnd = sLength - 1;
+        while (currentStart <= currentEnd){
+            if (s.charAt(currentStart) != s.charAt(currentEnd)){
+                return false;
+            }
+            ++currentStart;
+            --currentEnd;
+        }
+        return true;
+    }
+
+    // Method3: Cool =))))
+    // Idea: Valid Palindrome means the reverse preprocessed string equals to the initial preprocess string
+    // StringBuffer --> object for handling string (mutable string) --> add, delete, edit without creating new object
+    // String --> is immutable
+    public boolean isPalindrome_3(String s) {
+        String regex = "[^a-zA-z0-9]";
+        String preprocessed = s.trim().toLowerCase().replaceAll(regex, "");
+        String rev = new StringBuffer(preprocessed).reverse().toString(); // why we must have toString() method here although rev is string ?
+        if (rev.equals(preprocessed)){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+
+
     public static void main(String []args){
         ValidPalindrome_Ti sol = new ValidPalindrome_Ti();
 
         String s1 = "H";
         System.out.println("s1: " + sol.isPalindrome(s1)); // true
+        System.out.println("s1: " + sol.isPalindrome_2(s1)); // true
+        System.out.println("s1: " + sol.isPalindrome_3(s1)); // true
         String s2 = "Was it a car or a cat I saw?";
         System.out.println("s2: " + sol.isPalindrome(s2)); // true
+        System.out.println("s2: " + sol.isPalindrome_2(s2)); // true
+        System.out.println("s2: " + sol.isPalindrome_3(s2)); // true
         String s3 = "tab a cat";
         System.out.println("s3: " + sol.isPalindrome(s3)); // false
+        System.out.println("s3: " + sol.isPalindrome_2(s3)); // false
+        System.out.println("s3: " + sol.isPalindrome_3(s3)); // false
         String s4 = "Was it a car or a cat I saw?";
         System.out.println("s4: " + sol.isPalindrome(s4)); // true
+        System.out.println("s4: " + sol.isPalindrome_2(s4)); // true
+        System.out.println("s4: " + sol.isPalindrome_3(s4)); // true
         String s5 = "tab@@@ a# cat&*)*^[]+-*/~`(&()*&";
         System.out.println("s5: " + sol.isPalindrome(s5)); // false
+        System.out.println("s5: " + sol.isPalindrome_2(s5)); // false
+        System.out.println("s5: " + sol.isPalindrome_3(s5)); // false
 
 
     }
--- src/test/java/com/leetcode/arrays/ValidPalindrome_TiTest.java
+++ src/test/java/com/leetcode/arrays/ValidPalindrome_TiTest.java
@@ -19,29 +19,41 @@ class ValidPalindrome_TiTest {
     void isPalindrome_SingleCharacter() {
         String s = "H";
         Assertions.assertTrue(this.solution.isPalindrome(s), "String \"H\" must return true");
+        Assertions.assertTrue(this.solution.isPalindrome_2(s), "String \"H\" must return true");
+        Assertions.assertTrue(this.solution.isPalindrome_3(s), "String \"H\" must return true");
     }
     // Case2: Valid Palindrome
     @Test
     void isPalindrome_Valid() {
         String s = "Was it a car or a cat I saw?";
         Assertions.assertTrue(this.solution.isPalindrome(s), "String \"Was it a car or a cat I saw?\" must return true");
+        Assertions.assertTrue(this.solution.isPalindrome_2(s), "String \"Was it a car or a cat I saw?\" must return true");
+        Assertions.assertTrue(this.solution.isPalindrome_3(s), "String \"Was it a car or a cat I saw?\" must return true");
     }
     // Case3: Invalid Palindrome
     @Test
     void isPalindrome_Invalid() {
         String s = "tab a cat";
         Assertions.assertFalse(this.solution.isPalindrome(s), "String \"tab a cat\" must return false");
+        Assertions.assertFalse(this.solution.isPalindrome_2(s), "String \"tab a cat\" must return false");
+        Assertions.assertFalse(this.solution.isPalindrome_3(s), "String \"tab a cat\" must return false");
     }
     // Case4: Valid Palindrome with non-alphanumeric characters
     @Test
     void isPalindrome_ValidWithNonAlphanumericCharacters() {
         String s = "Was it a car or a cat I saw?";
         Assertions.assertTrue(this.solution.isPalindrome(s), "String \"Was it$$$ a car !!!! or a cat *** I saw?\" must return true");
+        Assertions.assertTrue(this.solution.isPalindrome_2(s), "String \"Was it$$$ a car !!!! or a cat *** I saw?\" must return true");
+        Assertions.assertTrue(this.solution.isPalindrome_3(s), "String \"Was it$$$ a car !!!! or a cat *** I saw?\" must return true");
+
     }
     // Case5: Invalid Palindrome with non-alphanumeric characters
     @Test
     void isPalindrome_InvalidWithNonAlphanumericCharacters() {
         String s = "tab@@@ a# cat&*)*^[]+-*/~`(&()*&";
         Assertions.assertFalse(this.solution.isPalindrome(s), "String \"tab a cat\" must return false");
+        Assertions.assertFalse(this.solution.isPalindrome_2(s), "String \"tab a cat\" must return false");
+        Assertions.assertFalse(this.solution.isPalindrome_3(s), "String \"tab a cat\" must return false");
+
     }
 }
