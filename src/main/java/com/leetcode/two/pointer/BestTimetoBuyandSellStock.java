4682891add4c11092350cc888a575aebae1e8679
tinabizone <thuy.tran@bizone.com.vn>
2025-06-04 22:45:38 +0700

    BestTimetoBuyandSellStock

---
+++ src/main/java/com/leetcode/two/pointer/BestTimetoBuyandSellStock.java
@@ -0,0 +1,28 @@
+package com.leetcode.two.pointer;
+
+public class BestTimetoBuyandSellStock {
+    //qua de voi Thi thi
+    //Cau hoi 1: ngay mai dc an mon gi zay aaaaaaaaaa
+    public static int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) return 0;
+
+        int maxProfit = 0;
+        int min = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < min) {
+
+                min = prices[i];
+
+            }
+            int profit = prices[i] - min;
+            maxProfit = Math.max(maxProfit, profit);
+        }
+        return maxProfit ;
+
+    }
+
+    public static void main(String[] args) {
+        int[] prices = { 10,8,7,5,2};
+        System.out.println(maxProfit(prices));
+    }
+}
