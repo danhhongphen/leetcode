f3a3aaf484f8e87dda8d5ad4d4591358ebc313ef
danhhongphen <primarydemon@gmail.com>
2025-06-05 09:51:12 +0700

    QA-MaxProfit_Ti

--- src/main/java/com/leetcode/arrays/BuyAndSellCrypto_Ti.java
+++ src/main/java/com/leetcode/arrays/BuyAndSellCrypto_Ti.java
@@ -4,14 +4,16 @@ public class BuyAndSellCrypto_Ti {
     // First method
     // Time complexity: O (n^2) ~~ Bruteforce
     public int maxProfit(int[] prices) {
-        int buyLowest = prices[0];
-        int saleHighest = prices[1];
+
+        //check input
+        int buyLowest = prices[0]; //please remove if unuse
+        int saleHighest = prices[1]; //please remove if unuse
         int pricesSize = prices.length;
         int profit = 0; // dont' know if any transactions will make profit --> in this case profi would be 0
-        for (int i = 0; i < pricesSize - 1; ++i){
-            for (int j = i + 1; j < pricesSize; ++j){
+        for (int i = 0; i < pricesSize - 1; ++i){ //add space
+            for (int j = i + 1; j < pricesSize; ++j){ //add space
                 int tmp = 0;
-                if (prices[j] > prices[i])
+                if (prices[j] > prices[i]) //please use only one format for {}
                 {
                     tmp = prices[j] - prices[i];
                     if (tmp > profit)
@@ -25,10 +27,12 @@ public class BuyAndSellCrypto_Ti {
     }
     // Mehthod: super simple but i don't know why i can not think like this :"(
     public int maxProfit_2(int[] prices) {
-        int buy_price = prices[0];
+        // check input
+
+        int buy_price = prices[0]; // do not naming like that -> follow camelCase
         int profit = 0;
-        for (int i = 1; i < prices.length; ++i){
-            if (prices[i] < buy_price)
+        for (int i = 1; i < prices.length; ++i){ // add space
+            if (prices[i] < buy_price) //please use only one format for {}
             {
                 buy_price = prices[i];
             }
