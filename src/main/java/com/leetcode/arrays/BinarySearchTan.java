f00bf378162ad7532f3ed1713dc1d223cd6f6028
danhhongphen <primarydemon@gmail.com>
2025-06-10 09:12:47 +0700

    QA-BinarySearch_Tan

--- src/main/java/com/leetcode/arrays/BinarySearchTan.java
+++ src/main/java/com/leetcode/arrays/BinarySearchTan.java
@@ -19,26 +19,21 @@ class Solution {
     }
 
     public int search1(int[] nums, int target) {
-       int l=0;
-       int r=nums.length-1;
-       while (l<=r)
-       {
-       int mid = l + (r - l) / 2;
-        if(nums[mid]==target)
-        {
-            return mid;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            }
         }
-        else if(nums[mid]<target)
-        {
-            l=mid+1;
-        }
-        else if(nums[mid]>target)
-        {
-            r=mid-1;
-        }
-       }
-       return -1;
+        return -1;
     }
+
     public static void main(String[] args) {
         Solution sol = new Solution();
 
