98190382ec9cf12ad2e63d4ef02dbea970f66aee
Trần Đình Tân <118046109+dinhtan131@users.noreply.github.com>
2025-06-11 18:34:05 +0700

    Update ReverseLinkedList.java

--- src/main/java/com/leetcode/utils/ReverseLinkedList.java
+++ src/main/java/com/leetcode/utils/ReverseLinkedList.java
@@ -1,5 +1,14 @@
 package com.leetcode.utils;
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 public class ReverseLinkedList {
 
     public ListNode reverseList(ListNode head) {
