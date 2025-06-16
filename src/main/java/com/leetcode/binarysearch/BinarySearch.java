2ea41c4c0290bc218a85f3ee99fef6233eacb042
danhhongphen <primarydemon@gmail.com>
2025-06-16 11:07:18 +0700

    binary search thuy thuy

--- src/main/java/com/leetcode/binarysearch/BinarySearch.java
+++ src/main/java/com/leetcode/binarysearch/BinarySearch.java
@@ -1,26 +1,35 @@
 package com.leetcode.binarysearch;
 
 public class BinarySearch {
-    public static int binarySearch(int [] arr, int target){
-        int res = -1;
+    public static int search(int [] arr, int target){
+        int left = 0;
+        int right = arr.length - 1;
+        binarySearch(arr, target, left, right);
+        return -1;
+    }
 
-        int index = arr.length /2;
-        while (index < arr.length ) {
-            if (target > arr[index]) {
-                index = index * 2;
-            }
-            if (target < arr[index]) {
-                index = index / 2;
-            }
-            if(target == arr[index]){
-                return  index;
-            }
+    public static int binarySearch(int[] arr, int target, int left, int right) {
+        if (left >= right) {
+            return -1;
         }
-        return index;
+
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target) {
+            return mid;
+        }
+        else if (arr[mid] < target) {
+            return binarySearch(arr, target, mid + 1, right);
+        }
+        else {
+            return binarySearch(arr, target, left, mid - 1);
+        }
     }
 
     public static void main(String[] args) {
-        int[] arr= {2,3,4,5,6,7,8,9,12,23,34,56,78,99};
-        System.out.println(binarySearch(arr,6));
+        int[] arr= {2,3,4,5,6,7,8};
+        System.out.println(search(arr,6));
     }
+
+
 }
